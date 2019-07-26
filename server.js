4af5e55c5c514cb8bbc6700be5f4b41abe3db5d9
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-07-27 00:23:07 +0545

    Resolve identifiers

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 const SocketManager = require('./manageSocket');
 
 
-const db = require('./config/keys').MONGO_URI;
+const db = require('./configuration/keys').MONGO_URI;
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log("Connected to the mongoose from real time server"))
