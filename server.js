fb7b590ef476e9c665c78b7f52ce936aec5ce6fd
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2019-08-03 01:54:19 +0545

    Update few messages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer();
 var io = module.exports.io = require('socket.io')(app);
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3231
+const PORT = process.env.PORT || 3233
 
 const SocketManager = require('./manageSocket');
 
@@ -10,12 +10,12 @@ const SocketManager = require('./manageSocket');
 const db = require('./configuration/keys').MONGO_URI;
 mongoose
 	.connect(db, { useNewUrlParser: true })
-	.then(() => console.log("Connected to the mongoose from rtServer"))
+	.then(() => console.log("Connected to the mongoose from real time server"))
 	.catch(err => console.log(err));
 
 
 io.on('connection', SocketManager);
 
 app.listen(PORT, () => {
-	console.log("Connected to port:" + PORT);
-})	
+	console.log("Listenning on real time server at port:" + PORT);
+})			
